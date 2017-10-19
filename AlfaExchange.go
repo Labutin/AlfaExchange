dba0d5987c7a6a5f213a9dfd4a729500992d3822
Dmitry Labutin <dlabutin@medallia.com>
2017-10-19 13:02:11 -0700

    Don't use http2 protocol

--- AlfaExchange.go
+++ AlfaExchange.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os/user"
 	"os"
-	"io/ioutil"
+	"os/user"
 	"strconv"
 )
 
@@ -20,29 +21,29 @@ type CurrencyInfo struct {
 
 type AlfaExchange struct {
 	Request struct {
-			Filter struct {
-				       Segment string `json:"segment"`
-				       Text    string `json:"text"`
-			       } `json:"filter"`
-			Limit   string `json:"limit"`
-			Offset  string `json:"offset"`
-			Order   string `json:"order"`
-			Server  string `json:"server"`
-			Service string `json:"service"`
-			Version string `json:"version"`
-		} `json:"request"`
+		Filter struct {
+			Segment string `json:"segment"`
+			Text    string `json:"text"`
+		} `json:"filter"`
+		Limit   string `json:"limit"`
+		Offset  string `json:"offset"`
+		Order   string `json:"order"`
+		Server  string `json:"server"`
+		Service string `json:"service"`
+		Version string `json:"version"`
+	} `json:"request"`
 	Response struct {
-			Data struct {
-				     Chf []CurrencyInfo `json:"chf"`
-				     Eur []CurrencyInfo `json:"eur"`
-				     Gbp []CurrencyInfo `json:"gbp"`
-				     Usd []CurrencyInfo `json:"usd"`
-			     } `json:"data"`
-			Status string `json:"status"`
-		} `json:"response"`
+		Data struct {
+			Chf []CurrencyInfo `json:"chf"`
+			Eur []CurrencyInfo `json:"eur"`
+			Gbp []CurrencyInfo `json:"gbp"`
+			Usd []CurrencyInfo `json:"usd"`
+		} `json:"data"`
+		Status string `json:"status"`
+	} `json:"response"`
 }
 
-func getStoreFilePath() (dir string, fileName string)  {
+func getStoreFilePath() (dir string, fileName string) {
 	usr, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
@@ -57,14 +58,14 @@ func createStoreFileIfNotExist(storePath string, fileName string) {
 	}
 	if _, err := os.Stat(storePath + fileName); os.IsNotExist(err) {
 		initValue := []byte("100000")
-		err := ioutil.WriteFile(storePath + fileName, initValue, 0600)
+		err := ioutil.WriteFile(storePath+fileName, initValue, 0600)
 		if err != nil {
 			log.Fatalf("Can't create store file %v", err)
 		}
 	}
 }
 
-func getSeenMinimum() (float64) {
+func getSeenMinimum() float64 {
 	storePath, storeFileName := getStoreFilePath()
 
 	createStoreFileIfNotExist(storePath, storeFileName)
@@ -84,13 +85,16 @@ func getSeenMinimum() (float64) {
 func updateSeenMinimum(newMinimum float64) {
 	initValue := []byte(strconv.FormatFloat(newMinimum, 'f', 2, 64))
 	storePath, storeFileName := getStoreFilePath()
-	err := ioutil.WriteFile(storePath + storeFileName, initValue, 0644)
+	err := ioutil.WriteFile(storePath+storeFileName, initValue, 0644)
 	if err != nil {
 		log.Fatalf("Can't write to store file %v", err)
 	}
 }
 
 func main() {
+	http.DefaultClient.Transport = &http.Transport{
+		TLSNextProto: make(map[string]func(authority string, c *tls.Conn) http.RoundTripper),
+	}
 	resp, err := http.Get("https://alfabank.ru/ext-json/0.2/exchange/cash/")
 	if err != nil {
 		log.Fatalf("Can't get rates %v", err)
@@ -106,7 +110,7 @@ func main() {
 	var sellOrder string
 	var buyPrice float64
 	var buyOrder string
-	for i:= range m.Response.Data.Usd {
+	for i := range m.Response.Data.Usd {
 		if m.Response.Data.Usd[i].Type == "sell" {
 			sellPrice = m.Response.Data.Usd[i].Value
 			sellOrder = m.Response.Data.Usd[i].Order
@@ -131,5 +135,3 @@ func main() {
 	}
 	fmt.Printf(outputFormat, buyOrder, buyPrice, sellOrder, sellPrice)
 }
-
-
